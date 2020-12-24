3b9fdb240067e5d6bb45581e00a107d3d6c3681a
Hellacious1702 <67594270+Hellacious1702@users.noreply.github.com>
2020-12-24 19:51:27 +0530

    my first commit to make a slingshot

---
+++ Slingshot.js
@@ -0,0 +1,21 @@
+class Slingshot {
+    constructor (bodyA , pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.4 
+        }
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    display(){
+        if(this.sling.bodyA !== null){
+            line(this.sling.bodyA.position.x, this.sling.bodyA.position.y, this.pointB.x, this.pointB.y);
+        }
+    }
+
+    fly(){
+        this.sling.bodyA = null ;
+    }
+}
